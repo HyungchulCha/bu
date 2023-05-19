84ab4d7e0a5e9bb2b1b3327d4979db73b3f8ae4d
ckgudcjf <ckgudcjf@gmail.com>
2023-05-19 10:25:29 +0900

    ing

--- ubt.py
+++ ubt.py
@@ -64,9 +64,9 @@ class BotUpbit():
         self.b_l = list(set(self.q_l + bal_lst))
         self.r_l = list(set(bal_lst).difference(self.q_l))
         self.prc_ttl = prc_ttl if prc_ttl < self.const_up else self.const_up
-        self.prc_ttl = 9500000
+        self.prc_ttl = 5000000
         self.prc_lmt = prc_lmt if prc_ttl < self.const_up else prc_lmt - (prc_ttl - self.const_up)
-        prc_buy = self.prc_ttl / (len(self.q_l) * 3.5)
+        prc_buy = self.prc_ttl / (len(self.q_l) * 3)
         self.prc_buy = prc_buy if prc_buy > self.const_dn else self.const_dn
 
         if os.path.isfile(FILE_URL_TIKR_3M):
